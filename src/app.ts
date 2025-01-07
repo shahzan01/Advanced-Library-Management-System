6876a77f3983679554bede83a21fcf4dda25816e
shahzan01 <m.shahzan2003@gmail.com>
2025-01-07 19:09:36 +0530

    y

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { auth } from "./middlewares/auth";
+import userRouter from "./routes/userRoutes";
+import bookRoute from "./routes/bookRoutes";
+import categoryRoute from "./routes/categoriesRoutes";
+import authorRoute from "./routes/authorRoutes";
+import libraryRoutes from "./routes/lidraryRoutes";
+import invoiceRoutes from "./routes/invoiceRoutes";
+import { apiRateLimiter } from "./middlewares/rateLimiter";
+import { scheduleReturnReminders } from "./utils/returnReminderScheduler";
+import cors from "cors";
+
+dotenv.config();
+scheduleReturnReminders();
+const app = express();
+app.use(express.json());
+app.use(apiRateLimiter);
+app.use(cors());
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello there" });
+});
+
+app.use("/api/v1/book", bookRoute);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/author", authorRoute);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/library", libraryRoutes);
+app.use("/api/v1/invoice", invoiceRoutes);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,7 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-import { auth } from "./middlewares/auth";
-import userRouter from "./routes/userRoutes";
-import bookRoute from "./routes/bookRoutes";
-import categoryRoute from "./routes/categoriesRoutes";
-import authorRoute from "./routes/authorRoutes";
-import libraryRoutes from "./routes/lidraryRoutes";
-import invoiceRoutes from "./routes/invoiceRoutes";
-import { apiRateLimiter } from "./middlewares/rateLimiter";
-import { scheduleReturnReminders } from "./utils/returnReminderScheduler";
-import cors from "cors";
+import app from "./app";
 
-dotenv.config();
-scheduleReturnReminders();
-const app = express();
-app.use(express.json());
-app.use(apiRateLimiter);
-app.use(cors());
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "hello there" });
+const port = process.env.PORT || 3000;
+console.log(port);
+app.listen(port, () => {
+  console.log(`Listening: http://localhost:${port}`);
 });
-
-app.use("/api/v1/book", bookRoute);
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/author", authorRoute);
-app.use("/api/v1/category", categoryRoute);
-app.use("/api/v1/library", libraryRoutes);
-app.use("/api/v1/invoice", invoiceRoutes);
-
-app.listen(3000);
